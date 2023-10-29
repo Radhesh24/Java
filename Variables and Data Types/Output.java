e1597e5b8547b1b7c2aeccd5da62300fd7e79f8c
Radhesh24 <110319408+Radhesh24@users.noreply.github.com>
2023-10-30 00:36:10 +0530

    Add files via upload

---
+++ Variables and Data Types/Output.java
@@ -0,0 +1,29 @@
+public class Output{
+    public static void main(String[] args) {
+
+
+    
+        System.out.print("This is my first Java Code");     //print is a function used to get output
+        System.out.println("My name is Radhesh");
+
+
+        System.out.println("Radhesh"); // println is used for line break
+        System.out.println("Khaire");
+
+        System.out.print("Amisha\n"); // '\n' is a escape sequence used for line break
+        System.out.print("Sonone\n");
+
+
+
+
+        // Printing a star pattern 
+
+        System.out.println("****");
+        System.out.println("***");
+        System.out.println("**");
+        System.out.println("*");
+
+
+    }
+    
+}
