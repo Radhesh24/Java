070addfc7a3c9d00c710cd350b462a8482e12d2f
Radhesh24 <110319408+Radhesh24@users.noreply.github.com>
2023-11-02 18:40:14 +0530

    Add files via upload

---
+++ Variables and Data Types/Input.java
@@ -0,0 +1,29 @@
+import java.util.*;
+public class Input{
+    public static void main(String args[]){
+
+        Scanner sc = new Scanner(System.in);
+
+
+
+        // .next() -->   Prints only a single word
+
+        // String input = sc.next();  
+        // System.out.println(input);
+
+
+        // .nextLine() -->   Prints the whole Line or Statement
+
+        // String input = sc.nextLine();
+        // System.out.println(input);
+
+
+        // .nextInt() -->   Prints Integers
+
+        int input = sc.nextInt();
+        System.out.println(input);
+
+
+
+    }
+}
