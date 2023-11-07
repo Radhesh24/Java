6a0c96df4556b42aafcd4776273c8fddad819fea
Radhesh24 <110319408+Radhesh24@users.noreply.github.com>
2023-11-07 23:51:04 +0530

    Add files via upload

---
+++ Conditional_Statements/largest_of_2.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class largest_of_2{
+
+    public static void main(String args[]){
+
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        if (a > b){
+            System.out.println("A is greater than B");
+        }
+        else {
+            System.out.println("B is greater than A");
+        }
+
+        
+    }
+}
