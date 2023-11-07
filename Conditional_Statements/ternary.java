bbfd476fe70944b5e08b1db0fa166a6549a33d23
Radhesh24 <110319408+Radhesh24@users.noreply.github.com>
2023-11-07 23:52:20 +0530

    Add files via upload

---
+++ Conditional_Statements/ternary.java
@@ -0,0 +1,13 @@
+
+
+public class ternary {
+    public static void main(String args[]){
+
+        // Ternary operator are used as single line if else 
+        // dtype name = condition? statement1 : statement2
+
+        String a = (5 % 3 == 0)? "Even":"Odd";
+        System.out.println(a); 
+    }
+    
+}
