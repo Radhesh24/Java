2aea5a2042b9a0b6ca574ab415dd1a3c9ee552c7
Radhesh24 <110319408+Radhesh24@users.noreply.github.com>
2023-11-08 22:19:14 +0530

    Add files via upload

---
+++ Loops/while_loop.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class while_loop {
+    public static void main(String args[]){
+
+        // While loop executes itself until a certain condition is true
+
+        Scanner sc = new Scanner(System.in);
+
+        int i = 1;
+        while (i <= 10){
+            System.out.println(i);
+            i++;
+        }
+        
+        int n = sc.nextInt();
+        int a = 1;
+        while (a <= n){
+            System.out.println(a);
+            a++;
+        }
+
+        
+        
+
+        }
+    }
+
