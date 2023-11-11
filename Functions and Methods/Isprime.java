428d2c7324d4ba5c1e4eef43765dc598e8337b8b
Radhesh24 <110319408+Radhesh24@users.noreply.github.com>
2023-11-11 22:03:53 +0530

    Add files via upload

---
+++ Functions and Methods/Isprime.java
@@ -0,0 +1,23 @@
+public class Isprime {
+
+    public static boolean isprime(int n){
+        if (n == 2){
+            return true;
+        }
+        for(int i = 2; i < n; i++){
+
+            if (n % i == 0){
+                return false;
+            }
+
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(isprime(9));
+        
+    }
+    
+}
