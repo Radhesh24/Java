427036fd0753ca11f431dd655a2b4f5da5181bbe
Radhesh24 <110319408+Radhesh24@users.noreply.github.com>
2023-11-22 23:40:21 +0530

    Add files via upload

---
+++ Patterns 2/palindromic_no.java
@@ -0,0 +1,28 @@
+public class palindromic_no {
+    public static void pali_no(int n ){
+
+        for(int i = 1; i <= n; i++){
+
+            for(int j = 1; j<= n-i; j++){
+
+                System.out.print(" ");
+            }
+            for(int j = i; j >= 1; j--){
+
+                System.out.print(j);
+            }
+            
+            for(int j = 2; j<= i ;j++){
+
+                System.out.print(j);
+                
+            }
+            
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        
+        pali_no(3);
+    }
+}
