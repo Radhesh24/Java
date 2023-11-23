dd72fe3f0e13985d63583406e15348ae086135ca
Radhesh24 <110319408+Radhesh24@users.noreply.github.com>
2023-11-23 23:52:54 +0530

    Add files via upload

---
+++ Array/operations.java
@@ -0,0 +1,15 @@
+public class operations {
+    
+    public static void main(String args[]){
+
+        int numbers[] = new int[3];
+        numbers[0] = 20;
+        numbers[1] = 10;
+        numbers[2] = 25;
+
+        for(int i = 0; i< numbers.length; i++){
+
+            System.out.println(numbers[i]);
+        }
+    }
+}
